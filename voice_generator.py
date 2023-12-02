6d6a4a3111505f8cd203e2133736740b7233bde8
Somesh Mehra <someshmehra99@gmail.com>
2023-12-02 23:54:05 +0100

    Fix voice generator

--- voice_generator.py
+++ voice_generator.py
@@ -4,28 +4,36 @@ import os
 from elevenlabs import set_api_key
 set_api_key(os.getenv("ELEVENLABS_API_KEY"))
 
-from elevenlabs import clone, generate, play
+from elevenlabs import clone, generate, play, voices
 
 class ClonedVoice():
 
-    def __init__(self, name, descrption):
+    def __init__(self, name):
 
         self.name = name
-        self.description = description
-        self.voice = None
+        self.voice = self._get_voice_if_exists()
 
-    def clone_voice(self, files):
+    def _get_voice_if_exists(self):
 
+        cloned_voices = [v for v in voices() if v.category=='cloned' and v.name==self.name]
+        if not cloned_voices:
+            print('No existing cloned voice with this name found.')
+            return None
+        print('An existing cloned voice with this name was found. No need to re-clone.')
+        return cloned_voices[0]
+
+    def clone_voice(self, files, description):
+
+        assert self.voice is None, "Cloned voice already exists. Re-cloning not allowed."
         self.voice = clone(
             name=self.name,
-            description=self.description,
+            description=description,
             files=files,
         )
 
     def generate_audio(self, text, play_audio=True):
 
-        assert
+        assert self.voice is not None, "Voice not yet cloned"
         audio = generate(text=text, voice=self.voice)
         if play_audio:
             play(audio)
-        return audio
